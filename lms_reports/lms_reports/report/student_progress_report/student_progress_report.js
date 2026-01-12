1a142e821acc4baefc00d3d59e7392c602d3ad24
Gulinur-ux <ruziyevagulinur@gmail.com>
2026-01-12 16:52:58 +0500

    Fayllar yaartildi

--- lms_reports/lms_reports/report/student_progress_report/student_progress_report.js
+++ lms_reports/lms_reports/report/student_progress_report/student_progress_report.js
@@ -1,5 +1,19 @@
 // Copyright (c) 2026, LMS Reports and contributors
-// For license information, please see license.txt
+
+// GLOBAL CLICK EVENT (Bu qism xavfsizlik filtridan o'tadi va hamma uchun ishlaydi)
+$(document).on('click', '.toggle-header', function(e) {
+    // Agar ichidagi bironta link bosilsa, yopilib ketmasligi uchun
+    if ($(e.target).is('a') || $(e.target).closest('a').length) return;
+
+    var $card = $(this).closest('.student-item');
+    var $details = $card.find('.details-body');
+    
+    // Animatsiya bilan ochish/yopish
+    $details.slideToggle(200);
+    
+    // Iconni aylantirish
+    $card.toggleClass('is-open');
+});
 
 frappe.query_reports["Student Progress Report"] = {
     "filters": [
@@ -8,7 +22,7 @@ frappe.query_reports["Student Progress Report"] = {
             "label": __("Student"),
             "fieldtype": "Link",
             "options": "User",
-            "default": frappe.session.user if frappe.session.user != "Administrator" else ""
+            "default": frappe.session.user !== "Administrator" ? frappe.session.user : ""
         },
         {
             "fieldname": "course",
@@ -23,13 +37,7 @@ frappe.query_reports["Student Progress Report"] = {
             "options": "Course Lesson",
             "get_query": function () {
                 var course = frappe.query_report.get_filter_value("course");
-                if (course) {
-                    return {
-                        filters: {
-                            "course": course
-                        }
-                    };
-                }
+                if (course) return { filters: { "course": course } };
             }
         },
         {
@@ -37,5 +45,9 @@ frappe.query_reports["Student Progress Report"] = {
             "label": __("Completed Only"),
             "fieldtype": "Check"
         }
-    ]
+    ],
+    "onload": function(report) {
+        // Template ko'rinishini majburiy qilish
+        report.view_name = "Report Template";
+    }
 };
--- lms_reports/lms_reports/report/student_progress_report/student_progress_report.py
+++ lms_reports/lms_reports/report/student_progress_report/student_progress_report.py
@@ -1,122 +1,85 @@
 # Copyright (c) 2026, LMS Reports and contributors
-# For license information, please see license.txt
-
 import frappe
 from frappe import _
 
-
 def execute(filters=None):
-	columns = get_columns()
-	data = get_data(filters)
-	return columns, data
+    columns = get_columns()
+    raw_data = get_data(filters)
+    
+    grouped_data = {}
+    for row in raw_data:
+        # dict access fix
+        student = row.get("student")
+        if not student:
+            continue
+            
+        if student not in grouped_data:
+            grouped_data[student] = {
+                "student": student,
+                "student_name": row.get("student_name") or student,
+                "student_email": frappe.db.get_value("User", student, "email") or student,
+                "total_lessons": 0,
+                "completed_count": 0,
+                "progress": 0,
+                "last_activity": None,
+                "lessons": []
+            }
+        
+        grouped_data[student]["lessons"].append(row)
+        grouped_data[student]["total_lessons"] += 1
+        
+        if row.get("is_completed"):
+            grouped_data[student]["completed_count"] += 1
+            
+        # Mantiqiy tuzatish: Faqat completion emas, har qanday faollikni kuzatamiz
+        ts = row.get("last_watched_timestamp")
+        if ts:
+            if not grouped_data[student]["last_activity"] or ts > grouped_data[student]["last_activity"]:
+                grouped_data[student]["last_activity"] = ts
 
+    final_data = []
+    for st_id, info in grouped_data.items():
+        if info["total_lessons"] > 0:
+            info["progress"] = round((info["completed_count"] / info["total_lessons"]) * 100, 1)
+        
+        # Datetime formatlash
+        if info["last_activity"]:
+            info["last_activity_display"] = frappe.utils.format_datetime(info["last_activity"], "dd-MM-yyyy HH:mm:ss")
+        else:
+            info["last_activity_display"] = _("Never")
+            
+        final_data.append(info)
 
-def get_columns():
-	return [
-		{
-			"fieldname": "student",
-			"label": _("Student"),
-			"fieldtype": "Link",
-			"options": "User",
-			"width": 150
-		},
-		{
-			"fieldname": "student_name",
-			"label": _("Student Name"),
-			"fieldtype": "Data",
-			"width": 180
-		},
-		{
-			"fieldname": "course",
-			"label": _("Course"),
-			"fieldtype": "Link",
-			"options": "LMS Course",
-			"width": 150
-		},
-		{
-			"fieldname": "chapter",
-			"label": _("Chapter"),
-			"fieldtype": "Link",
-			"options": "Course Chapter",
-			"width": 150
-		},
-		{
-			"fieldname": "lesson",
-			"label": _("Lesson"),
-			"fieldtype": "Link",
-			"options": "Course Lesson",
-			"width": 180
-		},
-		{
-			"fieldname": "completion_percentage",
-			"label": _("Completion %"),
-			"fieldtype": "Percent",
-			"width": 110
-		},
-		{
-			"fieldname": "is_completed",
-			"label": _("Completed"),
-			"fieldtype": "Check",
-			"width": 80
-		},
-		{
-			"fieldname": "video_speed",
-			"label": _("Speed"),
-			"fieldtype": "Data",
-			"width": 80
-		},
-		{
-			"fieldname": "watched_duration",
-			"label": _("Watched (sec)"),
-			"fieldtype": "Float",
-			"width": 110
-		},
-		{
-			"fieldname": "last_watched_timestamp",
-			"label": _("Last Watched"),
-			"fieldtype": "Datetime",
-			"width": 150
-		},
-		{
-			"fieldname": "quiz_attempts",
-			"label": _("Attempts"),
-			"fieldtype": "Int",
-			"width": 100
-		},
-		{
-			"fieldname": "quiz_best_score",
-			"label": _("Best Score"),
-			"fieldtype": "Percent",
-			"width": 100
-		},
-		{
-			"fieldname": "quiz_passed_at_attempt",
-			"label": _("Passed At"),
-			"fieldtype": "Int",
-			"width": 100
-		}
-	]
-
+    return columns, final_data
 
 def get_data(filters):
-	conditions = {}
-	if filters.get("student"):
-		conditions["student"] = filters.get("student")
-	if filters.get("course"):
-		conditions["course"] = filters.get("course")
-	if filters.get("lesson"):
-		conditions["lesson"] = filters.get("lesson")
-	if filters.get("is_completed"):
-		conditions["is_completed"] = 1
+    conditions = {}
+    if filters.get("student"):
+        conditions["student"] = filters.get("student")
+    if filters.get("course"):
+        conditions["course"] = filters.get("course")
+    if filters.get("lesson"):
+        conditions["lesson"] = filters.get("lesson")
+    
+    # Filtr tuzatish
+    if filters.get("is_completed") == 1:
+        conditions["is_completed"] = 1
 
-	return frappe.get_all(
-		"LMS Student Lesson Log",
-		filters=conditions,
-		fields=[
-			"student", "student_name", "course", "chapter", "lesson",
-			"completion_percentage", "is_completed", "video_speed",
-			"watched_duration", "last_watched_timestamp",
-			"quiz_attempts", "quiz_best_score", "quiz_passed_at_attempt"
-		],
-		order_by="last_watched_timestamp desc"
-	)
+    return frappe.get_all(
+        "LMS Student Lesson Log",
+        filters=conditions,
+        fields=[
+            "student", "student_name", "course", "chapter", "lesson",
+            "completion_percentage", "is_completed", "video_speed",
+            "watched_duration", "last_watched_timestamp",
+            "quiz_attempts", "quiz_best_score"
+        ],
+        order_by="last_watched_timestamp desc"
+    )
+
+def get_columns():
+    return [
+        {"fieldname": "student", "label": _("Student"), "fieldtype": "Link", "options": "User", "width": 150},
+        {"fieldname": "student_name", "label": _("Student Name"), "fieldtype": "Data", "width": 150},
+        {"fieldname": "progress", "label": _("Total Progress %"), "fieldtype": "Percent", "width": 120}
+    ]
